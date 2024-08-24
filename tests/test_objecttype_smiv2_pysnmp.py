lextudio/pysmi

commit a42d1705375f3844ba27dbf734e9d8c3a83abc1f

Introduce Jinja2 templates for code generation (#41)
* Introduce Jinja2 templates for code generation

This change significantly refactors the way how code generation
works. Previously, pysmi code generator has been responsible for
producing a well-formed text document (e.g. JSON or pysnmp).

With this change, all code generations should be done through
Jinja2 templates by rendering them in the context of the parsed MIB
taking shape of the intermediate MIB representation which other parts
of pysmi provide.

By way of moving pysnmp code generation to Jinja2 template, the
Python code layout of pysnmp modules improved dramatically - it
is just one little step from being PEP8-compliant.

This template-based pysnmp code generator drops some backward
compatibility aids that keep Python MIB modules compatible with
older pysnmp versions. Perhaps in the followup patch we should
make the Python MIB module failing early and clearly on import
when it's old pysnmp importing it.

Jinja does not seem to work well with Python < 2.6 and Python == 3.2.
Despite pysmi is trying to support those Python versions, it may
start to fail on them due to Jinja failures.

tests/test_objecttype_smiv2_pysnmp.py
@@ -66,7 +66,7 @@ END
     def testObjectTypeDescription(self):
         self.assertEqual(
             self.ctx['testObjectType'].getDescription(),
-            'Test object',
+            'Test object\n',
             'bad DESCRIPTION'
         )
 
@@ -87,7 +87,7 @@ END
     def testObjectTypeMaxAccess(self):
         self.assertEqual(
             self.ctx['testObjectType'].getMaxAccess(),
-            'accessiblefornotify',
+            'accessible-for-notify',
             'bad MAX-ACCESS'
         )
 
@@ -217,6 +217,7 @@ END
 
         exec(codeobj, self.ctx, self.ctx)
 
+    # TODO: pyasn1 does not like OctetString.defaultValue
     def testObjectTypeSyntax(self):
         self.assertEqual(
             self.ctx['testObjectType'].getSyntax(), str2octs('test value'),

.travis.yml
@@ -8,9 +8,10 @@ matrix:
   - os: linux
     dist: trusty
     python: '2.7'
-  - os: linux
-    dist: trusty
-    python: '3.2'
+  # Jinja seems to be broken on py3.2
+#  - os: linux
+#    dist: trusty
+#    python: '3.2'
   - os: linux
     dist: trusty
     python: '3.3'

pysmi/codegen/templates/jsondoc.j2
@@ -1,0 +1,2 @@
+
+{{ mib|tojson(indent=2) }}

pysmi/codegen/templates/pysnmp/pysnmp.j2
@@ -1,0 +1,490 @@
+"""{{ mib['meta']['module'] }} SNMP MIB module compiled for pysnmp.
+
+This Python module is designed to be imported and executed by the
+pysnmp library.
+
+See http://snmplabs.com/pysnmp for further information.
+
+Notes
+-----
+{% for comment in mib['meta']['comments'] -%}
+{{ comment }}
+{% endfor -%}
+"""
+
+try:
+    mibBuilder
+
+except NameError:
+    import sys
+    sys.stderr.write(__doc__)
+    sys.exit(1)
+
+# Import base ASN.1 objects even if this MIB does not use it
+
+(Integer,
+ OctetString,
+ ObjectIdentifier) = mibBuilder.importSymbols("ASN1",
+    "Integer",
+    "OctetString",
+    "ObjectIdentifier")
+
+(NamedValues,) = mibBuilder.importSymbols("ASN1-ENUMERATION",
+    "NamedValues")
+
+(ConstraintsIntersection,
+ SingleValueConstraint,
+ ValueRangeConstraint,
+ ValueSizeConstraint,
+ ConstraintsUnion) = mibBuilder.importSymbols("ASN1-REFINEMENT",
+    "ConstraintsIntersection",
+    "SingleValueConstraint",
+    "ValueRangeConstraint",
+    "ValueSizeConstraint",
+    "ConstraintsUnion")
+
+# Import SMI symbols from the MIBs this MIB depends on
+
+{% for module, symbols in mib['imports'].items() %}
+    {% for symbol in symbols %}
+        {% if loop.first and loop.last %}
+({{ symbol|replace('-', '_') }},) = mibBuilder.importSymbols("{{ module }}",
+        {% elif loop.first %}
+({{ symbol|replace('-', '_') }},
+        {% elif loop.last %}
+ {{ symbol|replace('-', '_') }}) = mibBuilder.importSymbols("{{ module }}",
+        {%  else %}
+ {{ symbol|replace('-', '_') }},
+        {%  endif %}
+    {%  endfor %}
+    {% for symbol in symbols %}
+        {% if loop.last %}
+    "{{ symbol }}")
+        {% else %}
+    "{{ symbol }}",
+        {% endif %}
+    {%  endfor %}
+
+{%  endfor %}
+
+# MODULE-IDENTITY
+
+{% for symbol, definition in mib.items() if definition['class'] == 'moduleidentity' %}
+{{ symbol }} = ModuleIdentity(
+    {{ definition['oid'] }}
+)
+{{ symbol }}.setRevisions(
+    {% for revision in definition.get('revisions', ()) %}
+        {% if loop.first and loop.last %}
+    ("{{ revision['revision'] }}",)
+        {% elif loop.first %}
+    ("{{ revision['revision'] }}",
+        {% elif loop.last %}
+     "{{ revision['revision'] }}")
+        {%  else %}
+     "{{ revision['revision'] }}",
+        {%  endif %}
+    {%  endfor %}
+)
+    {% if 'lastupdated' in definition %}
+{{ symbol }}.setLastUpdated("{{ definition['lastupdated'] }}")
+    {% endif %}
+
+    {% if 'organization' in definition %}
+if mibBuilder.loadTexts:
+    {{ symbol }}.setOrganization("{{ definition['organization'] }}")
+    {% endif %}
+
+    {% if 'contactinfo' in definition %}
+{{ symbol }}.setContactInfo("{{ definition['contactinfo'] }}")
+    {% endif %}
+
+    {% if 'description' in definition %}
+if mibBuilder.loadTexts:
+    {{ symbol|replace('-', '_') }} = {{ symbol|replace('-', '_') }}.setDescription("""\
+{{ definition['description']|wordwrap }}
+""")
+    {% endif %}
+
+{%  endfor %}
+
+{% macro constraints(type, spec) %}
+    subtypeSpec = {{ type }}.subtypeSpec
+    {% if 'enumeration' in spec %}
+    subtypeSpec += ConstraintsUnion(
+        SingleValueConstraint(
+        {% for iden in spec['enumeration'].values()|sort %}
+            {% if loop.first and loop.last %}
+        {{ iden }}
+            {% elif loop.first %}
+        *({{ iden }},
+            {% elif loop.last %}
+          {{ iden }})
+            {% else %}
+          {{ iden }},
+            {% endif %}
+        {% endfor %}
+        )
+    )
+    namedValues = NamedValues(
+    {% for name, iden in spec['enumeration'].items()|sort %}
+        {% if loop.first and loop.last %}
+        ("{{ name}}", {{ iden }})
+        {% elif loop.first %}
+        *(("{{ name}}", {{ iden }}),
+        {% elif loop.last %}
+          ("{{ name}}", {{ iden }}))
+        {% else %}
+          ("{{ name}}", {{ iden }}),
+        {% endif %}
+    {% endfor %}
+    )
+    {% elif 'range' in spec %}
+    subtypeSpec += ConstraintsUnion(
+        {% for range in spec['range'] %}
+        ValueRangeConstraint({{ range['min'] }}, {{ range['max'] }}),
+        {% endfor %}
+    )
+    {% elif 'size' in spec %}
+    subtypeSpec += ConstraintsUnion(
+        {% for range in spec['size'] %}
+        ValueSizeConstraint({{ range['min'] }}, {{ range['max'] }}),
+        {% endfor %}
+    )
+    {% endif %}
+{% endmacro %}
+
+{% macro default(definition) %}
+    {% if definition['default']['default']['format'] == 'decimal' %}
+    defaultValue = {{ definition['default']['default']['value'] }}
+    {% elif definition['default']['default']['format'] == 'hex' %}
+    {# TODO: pyasn1 Integer does not have defaultHexValue #}
+    defaultHexValue = {{ definition['default']['default']['value'] }}
+    {# TODO: pyasn1 Integer does not have defaultBinValue #}
+    {% elif definition['default']['default']['format'] == 'bin' %}
+    defaultBinValue = {{ definition['default']['default']['value'] }}
+    {% elif definition['default']['default']['format'] == 'string' %}
+    {# TODO: pyasn1 does not like defaulted strings #}
+    defaultValue = OctetString("{{ definition['default']['default']['value'] }}")
+    {% elif definition['default']['default']['format'] == 'oid' %}
+    {# TODO: this OID might be in a string form #}
+    defaultValue = "{{ definition['default']['default']['value'] }}"
+    {% elif definition['default']['default']['format'] == 'enum' %}
+    {# TODO: pyasn1 does not like default enum #}
+    defaultValue = {{ definition['syntax']['constraints']['enumeration'][definition['default']['default']['value']] }}
+    {% elif definition['default']['default']['format'] == 'bits' %}
+    {# TODO: pyasn1 does not like default named bits #}
+    defaultValue = {{ definition['syntax']['constraints']['enumeration'][definition['default']['default']['value']] }}
+    {%  endif %}
+{% endmacro %}
+
+# Types definitions
+
+{% for symbol, definition in mib.items() if definition['class'] == 'type' %}
+
+class {{ symbol }}({{ definition['type']['type'] }}):
+    """Custom type {{ symbol }} based on {{ definition['type']['type'] }}"""
+    {% if 'default' in definition %}
+{{ default(definition) }}
+    {% endif %}
+    {% if 'constraints' in definition['type'] %}
+{{ constraints(definition['type']['type'], definition['type']['constraints']) }}
+    {% endif %}
+
+{%  endfor %}
+
+# TEXTUAL-CONVENTIONS
+
+{% for symbol, definition in mib.items() if definition['class'] == 'textualconvention' %}
+
+class {{ symbol }}(TextualConvention, {{ definition['type']['type'] }}):
+    status = "{{ definition.get('status', 'current') }}"
+    {% if 'displayhint' in definition %}
+    displayHint = "{{ definition['displayhint'] }}"
+    {% endif %}
+    {% if 'constraints' in definition['type'] %}
+{{ constraints(definition['type']['type'], definition['type']['constraints']) }}
+    {% endif %}
+    {% if 'description' in definition %}
+    if mibBuilder.loadTexts:
+        description = """\
+{{ definition['description']|wordwrap }}
+"""
+    {% endif %}
+
+{%  endfor %}
+
+# MIB Managed Objects in the order of their OIDs
+
+{% for symbol, definition in mib.items()
+   if definition['class'] in ('objecttype', 'objectidentity') %}
+    {% if 'syntax' in definition %}
+        {% if 'default' in definition or 'constraints' in definition['syntax'] or 'bits' in definition['syntax'] %}
+class _{{ symbol|replace('-', '_')|title }}Type({{ definition['syntax']['type'] }}):
+            {% if 'default' in definition %}
+{{ default(definition) }}
+            {% endif %}
+            {% if 'constraints' in definition['syntax'] %}
+{{ constraints(definition['syntax']['type'], definition['syntax']['constraints']) }}
+            {% endif %}
+            {% if 'bits' in definition['syntax'] %}
+    namedValues = NamedValues(
+                {% for name, iden in definition['syntax']['bits'].items()|sort %}
+                    {% if loop.first and loop.last %}
+        ("{{ name}}", {{ iden }})
+                    {% elif loop.first %}
+        *(("{{ name}}", {{ iden }}),
+                    {% elif loop.last %}
+          ("{{ name}}", {{ iden }}))
+                    {% else %}
+          ("{{ name}}", {{ iden }}),
+                    {% endif %}
+                {% endfor %}
+    )
+            {% endif %}
+            {% if 'constraints' in definition['syntax'] or 'bits' in definition['syntax'] %}
+_{{ symbol|replace('-', '_')|title }}Type.__name__ = "{{ definition['syntax']['type'] }}"
+            {% endif %}
+        {% else %}
+_{{ symbol|replace('-', '_')|title }}Type = {{ definition['syntax']['type'] }}
+        {% endif %}
+    {% endif %}
+    {% if definition['class'] == 'objectidentity' %}
+{{ symbol|replace('-', '_') }} = ObjectIdentity(
+    {{ definition['oid'] }}
+)
+    {% elif definition['nodetype'] == 'scalar' %}
+{{ symbol|replace('-', '_') }} = MibScalar(
+    {{ definition['oid'] }},
+    _{{ symbol|replace('-', '_')|title }}Type()
+)
+{{ symbol|replace('-', '_') }} = {{ symbol|replace('-', '_') }}.setMaxAccess("{{ definition['maxaccess'] }}")
+    {% elif definition['nodetype'] == 'table' %}
+{{ symbol|replace('-', '_') }} = MibTable(
+    {{ definition['oid'] }}
+)
+    {% elif definition['nodetype'] == 'row' %}
+{{ symbol|replace('-', '_') }} = MibTableRow(
+    {{ definition['oid'] }}
+)
+        {% if 'indices' in definition %}
+{{ symbol|replace('-', '_') }}.setIndexNames(
+            {% for index in definition['indices'] %}
+    ({{ index['implied'] }}, "{{ index['module'] }}", "{{ index['object'] }}"),
+            {% endfor %}
+)
+        {% endif %}
+        {% if 'augmention' in definition %}
+{{ definition['augmention']['object'] }}.registerAugmentions(
+    ("{{ mib['meta']['module'] }}",
+     "{{ symbol|replace('-', '_') }}")
+)
+{{ symbol|replace('-', '_') }}.setIndexNames(*{{ definition['augmention']['object'] }}.getIndexNames())
+        {% endif %}
+    {% elif definition['nodetype'] == 'column' %}
+{{ symbol|replace('-', '_') }} = MibTableColumn(
+    {{ definition['oid'] }},
+    _{{ symbol|replace('-', '_')|title }}Type()
+)
+{{ symbol|replace('-', '_') }} = {{ symbol|replace('-', '_') }}.setMaxAccess("{{ definition['maxaccess'] }}")
+    {% endif %}
+    {% if 'status' in definition %}
+if mibBuilder.loadTexts:
+    {{ symbol|replace('-', '_') }} = {{ symbol|replace('-', '_') }}.setStatus("{{ definition['status'] }}")
+    {% endif %}
+    {% if 'units' in definition %}
+if mibBuilder.loadTexts:
+    {{ symbol|replace('-', '_') }} = {{ symbol|replace('-', '_') }}.setUnits("{{ definition['units'] }}")
+    {% endif %}
+    {% if 'reference' in definition %}
+if mibBuilder.loadTexts:
+    {{ symbol|replace('-', '_') }} = {{ symbol|replace('-', '_') }}.setReference("{{ definition['reference'] }}")
+    {% endif %}
+    {% if 'description' in definition %}
+if mibBuilder.loadTexts:
+    {{ symbol|replace('-', '_') }} = {{ symbol|replace('-', '_') }}.setDescription("""\
+{{ definition['description']|wordwrap }}
+""")
+    {% endif %}
+
+{%  endfor %}
+
+# Managed Objects groups
+
+{% for symbol, definition in mib.items()
+   if definition['class'] == 'objectgroup' %}
+{{ symbol|replace('-', '_') }} = ObjectGroup(
+    {{ definition['oid'] }}
+)
+    {% if 'objects' in definition %}
+{{ symbol|replace('-', '_') }}.setObjects(
+    {% for obj in definition['objects'] %}
+        {% if loop.first and loop.last %}
+    ("{{ obj['module'] }}", "{{  obj['object'] }}")
+        {% elif loop.first %}
+      *(("{{ obj['module'] }}", "{{  obj['object'] }}"),
+        {% elif loop.last %}
+        ("{{ obj['module'] }}", "{{  obj['object'] }}"))
+        {% else %}
+        ("{{ obj['module'] }}", "{{  obj['object'] }}"),
+        {% endif %}
+    {% endfor %}
+)
+    {% endif %}
+if mibBuilder.loadTexts:
+    {{ symbol|replace('-', '_') }} = {{ symbol|replace('-', '_') }}.setStatus("{{ definition['status'] }}")
+    {% if 'description' in definition %}
+if mibBuilder.loadTexts:
+    {{ symbol|replace('-', '_') }} = {{ symbol|replace('-', '_') }}.setDescription("""\
+{{ definition['description']|wordwrap }}
+""")
+    {% endif %}
+
+{%  endfor %}
+
+# Notification objects
+
+{% for symbol, definition in mib.items()
+   if definition['class'] == 'notificationtype' %}
+{{ symbol|replace('-', '_') }} = NotificationType(
+    {{ definition['oid'] }}
+)
+    {% if 'objects' in definition %}
+{{ symbol|replace('-', '_') }}.setObjects(
+    {% for obj in definition['objects'] %}
+        {% if loop.first and loop.last %}
+    ("{{ obj['module'] }}", "{{  obj['object'] }}")
+        {% elif loop.first %}
+      *(("{{ obj['module'] }}", "{{  obj['object'] }}"),
+        {% elif loop.last %}
+        ("{{ obj['module'] }}", "{{  obj['object'] }}"))
+        {% else %}
+        ("{{ obj['module'] }}", "{{  obj['object'] }}"),
+        {% endif %}
+    {% endfor %}
+)
+    {% endif %}
+if mibBuilder.loadTexts:
+    {{ symbol|replace('-', '_') }} = {{ symbol|replace('-', '_') }}.setStatus("{{ definition['status'] }}")
+    {% if 'description' in definition %}
+if mibBuilder.loadTexts:
+    {{ symbol|replace('-', '_') }} = {{ symbol|replace('-', '_') }}.setDescription("""\
+{{ definition['description']|wordwrap }}
+""")
+    {% endif %}
+
+{%  endfor %}
+
+# Notifications groups
+
+{% for symbol, definition in mib.items()
+   if definition['class'] == 'notificationgroup' %}
+{{ symbol|replace('-', '_') }} = NotificationGroup(
+    {{ definition['oid'] }}
+)
+    {% if 'objects' in definition %}
+{{ symbol|replace('-', '_') }}.setObjects(
+    {% for obj in definition['objects'] %}
+        {% if loop.first and loop.last %}
+    ("{{ obj['module'] }}", "{{  obj['object'] }}")
+        {% elif loop.first %}
+      *(("{{ obj['module'] }}", "{{  obj['object'] }}"),
+        {% elif loop.last %}
+        ("{{ obj['module'] }}", "{{  obj['object'] }}"))
+        {% else %}
+        ("{{ obj['module'] }}", "{{  obj['object'] }}"),
+        {% endif %}
+    {% endfor %}
+)
+    {% endif %}
+if mibBuilder.loadTexts:
+    {{ symbol|replace('-', '_') }} = {{ symbol|replace('-', '_') }}.setStatus("{{ definition['status'] }}")
+    {% if 'description' in definition %}
+if mibBuilder.loadTexts:
+    {{ symbol|replace('-', '_') }} = {{ symbol|replace('-', '_') }}.setDescription("""\
+{{ definition['description']|wordwrap }}
+""")
+    {% endif %}
+
+{%  endfor %}
+
+# Agent capabilities
+
+{% for symbol, definition in mib.items()
+   if definition['class'] == 'agentcapabilities' %}
+{{ symbol|replace('-', '_') }} = AgentCapabilities(
+    {{ definition['oid'] }}
+)
+    {% if 'productrelease' in definition %}
+if mibBuilder.loadTexts:
+    {{ symbol|replace('-', '_') }} = {{ symbol|replace('-', '_') }}.setProductRelease("{{ definition['productrelease'] }}")
+    {% endif %}
+    {% if 'reference' in definition %}
+if mibBuilder.loadTexts:
+    {{ symbol|replace('-', '_') }} = {{ symbol|replace('-', '_') }}.setReference("{{ definition['reference'] }}")
+    {% endif %}
+if mibBuilder.loadTexts:
+    {{ symbol|replace('-', '_') }} = {{ symbol|replace('-', '_') }}.setStatus("{{ definition['status'] }}")
+    {% if 'description' in definition %}
+if mibBuilder.loadTexts:
+    {{ symbol|replace('-', '_') }} = {{ symbol|replace('-', '_') }}.setDescription("""\
+{{ definition['description']|wordwrap }}
+""")
+    {% endif %}
+
+{%  endfor %}
+
+# Module compliance
+
+{% for symbol, definition in mib.items()
+   if definition['class'] == 'modulecompliance' %}
+{{ symbol|replace('-', '_') }} = ModuleCompliance(
+    {{ definition['oid'] }}
+)
+    {% if 'objects' in definition %}
+{{ symbol|replace('-', '_') }}.setObjects(
+    {% for obj in definition['objects'] %}
+        {% if loop.first and loop.last %}
+    ("{{ obj['module'] }}", "{{  obj['object'] }}")
+        {% elif loop.first %}
+      *(("{{ obj['module'] }}", "{{  obj['object'] }}"),
+        {% elif loop.last %}
+        ("{{ obj['module'] }}", "{{  obj['object'] }}"))
+        {% else %}
+        ("{{ obj['module'] }}", "{{  obj['object'] }}"),
+        {% endif %}
+    {% endfor %}
+)
+    {% endif %}
+if mibBuilder.loadTexts:
+    {{ symbol|replace('-', '_') }} = {{ symbol|replace('-', '_') }}.setStatus("{{ definition['status'] }}")
+    {% if 'description' in definition %}
+if mibBuilder.loadTexts:
+    {{ symbol|replace('-', '_') }} = {{ symbol|replace('-', '_') }}.setDescription("""\
+{{ definition['description']|wordwrap }}
+""")
+    {% endif %}
+
+{%  endfor %}
+
+# Export all MIB objects to the MIB builder
+
+mibBuilder.exportSymbols(
+    "{{ mib['meta']['module'] }}",
+    {% for symbol, definition in mib.items()
+       if definition['class'] in ('moduleidentity', 'objecttype',
+                                  'agentcapabilities', 'moduleidentity',
+                                  'modulecompliance', 'notificationgroup',
+                                  'notificationtype', 'objectgroup',
+                                  'objectidentity', 'textualconvention') %}
+            {% if loop.first and loop.last %}
+    **{"{{ symbol }}": {{ symbol|replace('-', '_') }}}
+            {% elif loop.first %}
+    **{"{{ symbol }}": {{ symbol|replace('-', '_') }},
+            {% elif loop.last %}
+       "{{ symbol }}": {{ symbol|replace('-', '_') }}}
+            {% else %}
+       "{{ symbol }}": {{ symbol|replace('-', '_') }},
+            {% endif %}
+    {%  endfor %}
+)

tests/test_agentcapabilities_smiv2_pysnmp.py
@@ -82,7 +82,7 @@ END
     def testAgentCapabilitiesDescription(self):
         self.assertEqual(
             self.ctx['testCapability'].getDescription(),
-            'test capabilities',
+            'test capabilities\n',
             'bad DESCRIPTION'
         )
 

commit fc0312386657ef9d7f31e5964c2a458e75f4c7d2

Fix DEFVAL support for objects with Bits syntax
The switch to Jinja2 based code generation in git-a42d170 inadvertently
broke support for DEFVAL clauses of object types with syntax Bits. Ever
since, MIB files that made use of DEFVAL for Bits types would cause
pysmi's pysnmp code generation process to fail on an exception. JSON
code generation did work, but did not produce the intended output.

This commit fixes the DEFVAL support for Bits, thereby making various
previously uncompilable MIB files not only compile, but also produce the
correct default values for Bits objects for objects upon use. The commit
also adds a pair of regression test cases.

This commit leaves open the issue that a DEFVAL clause with an empty bit
set will result in object instances that have no default value.

Please note that as indicated above, this commit makes a small breaking
change to the JSON output format structure for Bits DEFVAL values.

pysmi/codegen/intermediate.py
@@ -762,8 +762,6 @@ class IntermediateCodeGen(AbstractCodeGen):
 
                 outDict.update(value=self.genBits([defvalBits])[1], format="bits")
 
-                return outDict
-
             else:
                 raise error.PySmiSemanticError(
                     f'unknown type "{defvalType}" for defval "{defval}" of symbol "{objname}"'

pysmi/codegen/jfilters.py
@@ -11,3 +11,11 @@ def capfirst(text):
         return text
 
     return text[0].upper() + text[1:]
+
+
+def bitstring(bits):
+    mask = sum(1 << bit for bit in bits)
+
+    # The left-most character of the returned string is for bit number zero,
+    # so reverse the bits, while also stripping off the "0b" prefix.
+    return bin(mask)[:1:-1]

pysmi/codegen/pysnmp.py
@@ -146,6 +146,7 @@ class PySnmpCodeGen(IntermediateCodeGen):
         )
 
         env.filters["capfirst"] = jfilters.capfirst
+        env.filters["bitstring"] = jfilters.bitstring
 
         try:
             tmpl = env.get_template(dstTemplate or self.TEMPLATE_NAME)

pysmi/codegen/templates/pysnmp/mib-definitions.j2
@@ -185,7 +185,7 @@ if mibBuilder.loadTexts:
     defaultValue = {{ definition['syntax']['constraints']['enumeration'][definition['default']['default']['value']] }}
     {% elif definition['default']['default']['format'] == 'bits' %}
     {# TODO: pyasn1 does not like default named bits #}
-    defaultValue = {{ definition['syntax']['constraints']['enumeration'][definition['default']['default']['value']] }}
+    defaultBinValue = "{{ definition['default']['default']['value']['bits'].values()|bitstring }}"
     {%  endif %}
 {% endmacro -%}
 

tests/test_objecttype_smiv2_pysnmp.py
@@ -361,6 +361,82 @@ class ObjectTypeBitsTestCase(unittest.TestCase):
         )
 
 
+class ObjectTypeBitsDefaultTestCase(unittest.TestCase):
+    """
+    TEST-MIB DEFINITIONS ::= BEGIN
+    IMPORTS
+      OBJECT-TYPE
+        FROM SNMPv2-SMI;
+
+    testObjectType OBJECT-TYPE
+        SYNTAX          BITS { present(0), absent(1), changed(2) }
+        MAX-ACCESS      read-only
+        STATUS          current
+        DESCRIPTION     "Test object"
+        DEFVAL          { { present, absent } }
+     ::= { 1 3 }
+
+    END
+    """
+
+    def setUp(self):
+        ast = parserFactory()().parse(self.__class__.__doc__)[0]
+        mibInfo, symtable = SymtableCodeGen().genCode(ast, {}, genTexts=True)
+        self.mibInfo, pycode = PySnmpCodeGen().genCode(
+            ast, {mibInfo.name: symtable}, genTexts=True
+        )
+        codeobj = compile(pycode, "test", "exec")
+
+        self.ctx = {"mibBuilder": MibBuilder()}
+
+        exec(codeobj, self.ctx, self.ctx)
+
+    def testObjectTypeSyntax(self):
+        self.assertEqual(
+            self.ctx["testObjectType"].getSyntax(),
+            bytes((0xC0,)),
+            "bad DEFVAL",
+        )
+
+
+class ObjectTypeBitsDefaultMultiOctetTestCase(unittest.TestCase):
+    """
+    TEST-MIB DEFINITIONS ::= BEGIN
+    IMPORTS
+      OBJECT-TYPE
+        FROM SNMPv2-SMI;
+
+    testObjectType OBJECT-TYPE
+        SYNTAX          BITS { a(0), b(1), c(2), d(3), e(4), f(5), g(6), h(7), i(8), j(9), k(10), l(11), m(12), n(13), o(14), p(15), q(16) }
+        MAX-ACCESS      read-only
+        STATUS          current
+        DESCRIPTION     "Test object"
+        DEFVAL          { { b, c, m } }
+     ::= { 1 3 }
+
+    END
+    """
+
+    def setUp(self):
+        ast = parserFactory()().parse(self.__class__.__doc__)[0]
+        mibInfo, symtable = SymtableCodeGen().genCode(ast, {}, genTexts=True)
+        self.mibInfo, pycode = PySnmpCodeGen().genCode(
+            ast, {mibInfo.name: symtable}, genTexts=True
+        )
+        codeobj = compile(pycode, "test", "exec")
+
+        self.ctx = {"mibBuilder": MibBuilder()}
+
+        exec(codeobj, self.ctx, self.ctx)
+
+    def testObjectTypeSyntax(self):
+        self.assertEqual(
+            self.ctx["testObjectType"].getSyntax(),
+            bytes((0x60, 0x08)),
+            "bad DEFVAL",
+        )
+
+
 class ObjectTypeMibTableTestCase(unittest.TestCase):
     """
     TEST-MIB DEFINITIONS ::= BEGIN